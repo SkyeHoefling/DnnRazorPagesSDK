b7cf188645bcd8cfe6979556babd054746b14d22
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-02-28 14:28:00 -0500

    Added DnnPageModel stub

---
+++ src/DotNetNuke.Web.Mvc.RazorPages.SDK/DnnPageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if NETCOREAPP2_0
+using Microsoft.AspNetCore.Mvc.RazorPages;
+#elif NET45
+using DotNetNuke.Web.Mvc.RazorPages.SDK.NETFramework;
+#endif
+
+namespace DotNetNuke.Web.Mvc.RazorPages.SDK
+{
+    public class DnnPageModel
+#if NECOREAPP2_0
+        : PageModel
+#elif NET45
+        : PageModel
+#endif
+    {
+    }
+}
